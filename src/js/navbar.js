09d0eb4452ec465a0ae8debd94a3f18297027294
REINA SULISTYANI ARIFIN <riemar0808@gmail.com>
2026-08-01 19:23:59 +0700

    ntah

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,11 +1,11 @@
 const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
-
+    if (!navbar) return;
     if (window.scrollY > 30) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
-
 });
+
